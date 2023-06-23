fb0a758a29df834637bcaf39293e5ff8a5cd1b87
IrynaPaltseva <130765340+IrynaPaltseva@users.noreply.github.com>
2023-06-23 11:54:11 -0400

    I want to commit new file

---
+++ src/myTestSeleniumPackage/NewClassIryna.java
@@ -0,0 +1,12 @@
+package myTestSeleniumPackage;
+
+public class NewClassIryna {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I want to commit new file");
+
+	}
+
+}
